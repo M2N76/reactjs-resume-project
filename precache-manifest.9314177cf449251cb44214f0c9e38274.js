b933b81fa0ff83fbe70f11467b7206824879c377
Korosh Aria <korosh.aria.m2n@gmail.com>
2020-10-02 01:45:11 +0330

    Updates

---
+++ precache-manifest.9314177cf449251cb44214f0c9e38274.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b3870e081bc1b3112de5479f48defc",
+    "url": "/reactjs-resume-project/index.html"
+  },
+  {
+    "revision": "9723b2ca8080d2e80021",
+    "url": "/reactjs-resume-project/static/css/main.ec82ec36.chunk.css"
+  },
+  {
+    "revision": "1ee5c2eda8f535d77998",
+    "url": "/reactjs-resume-project/static/js/2.9289cf01.chunk.js"
+  },
+  {
+    "revision": "e4d0b7b6ee221c9758b548593a6bc2ac",
+    "url": "/reactjs-resume-project/static/js/2.9289cf01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9723b2ca8080d2e80021",
+    "url": "/reactjs-resume-project/static/js/main.717a038a.chunk.js"
+  },
+  {
+    "revision": "7145b682d2f702f214eb",
+    "url": "/reactjs-resume-project/static/js/runtime-main.c209d58d.js"
+  }
+]);
